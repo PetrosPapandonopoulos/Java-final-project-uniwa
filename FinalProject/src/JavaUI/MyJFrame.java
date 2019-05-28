3cc7abfbea336fb6db7406a0835d6909caea819a
PetrosPapandonopoulos <51060590+PetrosPapandonopoulos@users.noreply.github.com>
2019-05-28 17:23:00 +0300

    Add files via upload

--- FinalProject/src/JavaUI/MyJFrame.java
+++ FinalProject/src/JavaUI/MyJFrame.java
@@ -1,18 +1,32 @@
 package JavaUI;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
-public class MyJFrame extends JFrame implements ActionListener {
+public class MyJFrame extends JFrame {
 
-
+    private String filePath;
+    private JFrame MyFrame;
     private JTextArea txtArea;
-    private JTextField path;
+    private JTextField fieldPath;
+    private JFileChooser chPath;
+    private JPanel btnPanel, panelTxt;
+    private JMenuBar mb;
+    private JMenu m1, m2;
+    private JMenuItem miNew, miOpen, miStats, miSave, miSaveAs, miClr, miClose;
+    private JButton btnOpen, btnClr, btnSave, btnSaveAs, btnChangeMode;
+    private Boolean fileExists=false;
+    private FileReader fileIn;
+    private FileWriter fileOut;
 
+    //Constructor
     public MyJFrame() {
-        JFrame MyFrame = new JFrame("Text editor");
+        //Create my own frame
+        MyFrame = new JFrame("Text editor");
 
         try {
             // Set metal look and feel
@@ -24,21 +38,22 @@ public class MyJFrame extends JFrame implements ActionListener {
         }
 
 
+
         // Create a menubar
-        JMenuBar mb = new JMenuBar();
+        mb = new JMenuBar();
 
         // Create a menu for file menu
-        JMenu m1 = new JMenu("File");
-        JMenu m2 = new JMenu("Edit");
+        m1 = new JMenu("File");
+        m2 = new JMenu("Edit");
 
         // Create File menu items
-        JMenuItem miNew = new JMenuItem("New");
-        JMenuItem miOpen = new JMenuItem("Open");
-        JMenuItem miStats = new JMenuItem("Statistics");
-        JMenuItem miSave = new JMenuItem("Save");
-        JMenuItem miSaveAs = new JMenuItem("Save as");
-        JMenuItem miClr = new JMenuItem("Clear");
-        JMenuItem miClose = new JMenuItem("Exit");
+         miNew = new JMenuItem("New");
+         miOpen = new JMenuItem("Open");
+         miStats = new JMenuItem("Statistics");
+         miSave = new JMenuItem("Save");
+         miSaveAs = new JMenuItem("Save as");
+         miClr = new JMenuItem("Clear");
+         miClose = new JMenuItem("Exit");
 
         //add JmenuItems to Jmenu
         m1.add(miNew);
@@ -49,12 +64,49 @@ public class MyJFrame extends JFrame implements ActionListener {
         m1.add(miClose);
 
         //add action listener
-        miOpen.addActionListener(this);
-        miSave.addActionListener(this);
-        miSaveAs.addActionListener(this);
-        miStats.addActionListener(this);
-        miClose.addActionListener(this);
-        miClr.addActionListener(this);
+        miNew.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                NewFile();
+            }
+        });
+        miOpen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OpenFile();
+            }
+        });
+        miSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SaveFile();
+
+            }
+        });
+        miSaveAs.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CopyFile();
+            }
+        });
+        miStats.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        miClose.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ExitApp();
+            }
+        });
+        miClr.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ClearAreaAndField();
+            }
+        });
 
         //add JmenuItems to Jmenu for edit
         m2.add(miClr);
@@ -66,29 +118,69 @@ public class MyJFrame extends JFrame implements ActionListener {
 
 
         //Create Panel and Buttons for the panel
-        JPanel btnPanel = new JPanel();
-        JButton btnOpen = new JButton("Open");
-        JButton btnClr = new JButton("Clear");
-        JButton btnSave = new JButton("Save");
-        JButton btnSaveAs = new JButton("copy");
-        JButton btnChangeMode = new JButton("Change mode");
+        btnPanel = new JPanel();
+        btnOpen = new JButton("Open");
+        btnClr = new JButton("Clear");
+        btnSave = new JButton("Save");
+        btnSaveAs = new JButton("copy");
+        //btnChangeMode = new JButton("Change mode"); under construction
+
+        btnOpen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OpenFile();
+            }
+        });
+
+        btnClr.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ClearAreaAndField();
+
+            }
+        });
+
+        btnSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SaveFile();
+
+            }
+        });
+
+        btnSaveAs.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CopyFile();
+            }
+        });
+
+        //Under construction
+        //btnChangeMode.addActionListener(new ActionListener() {
+            //@Override
+           // public void actionPerformed(ActionEvent e) {
+                //changeBckround();
+           // }
+       // });
+
+
 
         //add buttons to the panel
         btnPanel.add(btnOpen);
         btnPanel.add(btnClr);
         btnPanel.add(btnSave);
         btnPanel.add(btnSaveAs);
-        btnPanel.add(btnChangeMode);
+        //btnPanel.add(btnChangeMode); Under construction
 
 
-        //Creates panel manager, textfield for the path, text area and Scrollbar if needed
-        JPanel panelTxt = new JPanel(new BorderLayout());
-        JTextField path = new JTextField();
-        JTextArea txtArea = new JTextArea();
+        //Creates panel manager, textfield for the fieldPath, text area and Scrollbar if needed
+         panelTxt = new JPanel(new BorderLayout());
+         fieldPath = new JTextField();
+         txtArea = new JTextArea();
         JScrollPane sp = new JScrollPane();
 
         //add text field and area to the panel manager
-        panelTxt.add(path, BorderLayout.NORTH);
+        panelTxt.add(fieldPath, BorderLayout.NORTH);
         sp.setHorizontalScrollBarPolicy( JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         sp.setVerticalScrollBarPolicy( JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         sp.getViewport().add(txtArea);
@@ -97,7 +189,6 @@ public class MyJFrame extends JFrame implements ActionListener {
 
         //Create the  full manager Layout
         Box managerPanel = new Box(BoxLayout.Y_AXIS);
-
 
         managerPanel.add(btnPanel, BorderLayout.WEST);
         managerPanel.add(panelTxt);
@@ -110,8 +201,133 @@ public class MyJFrame extends JFrame implements ActionListener {
 
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        
+
+    private void NewFile(){
+        filePath = "";
+        fieldPath.setText("");
+        txtArea.setText("");
+        fileExists = false;
     }
+
+   // private void changeBckround(){  **UNDER CONSTRUCTION :(**
+       // fieldPath.setBackground(Color.BLACK);
+      //  txtArea.setBackground(Color.BLACK);
+  //  }
+
+    private void OpenFile(){
+        chPath = new JFileChooser();
+        BufferedReader objReader;
+        String temp, str;
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Documents (*.txt)", "txt", "text");
+        chPath.setFileFilter(filter);
+        int result = chPath.showOpenDialog(null);
+        if(result ==JFileChooser.APPROVE_OPTION){
+            filePath = chPath.getSelectedFile().getAbsolutePath();
+            fieldPath.setText(filePath);
+            fileExists=true;
+            try {
+                fileIn = new FileReader(chPath.getSelectedFile());
+                objReader = new BufferedReader(fileIn);
+                temp = objReader.readLine();
+                while ((str = objReader.readLine()) != null) {
+                    temp += "\n" + str;
+                }
+                txtArea.setText(temp);
+                fileIn.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void SaveFile(){
+
+        if(fileExists){
+            try {
+                String temp;
+                BufferedWriter objWriter;
+                fileOut = new FileWriter(filePath);
+                objWriter = new BufferedWriter(fileOut);
+                temp = txtArea.getText();
+                PrintWriter pw = new PrintWriter(objWriter);
+                pw.print(temp);
+                pw.close();
+                fileOut.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            chPath = new JFileChooser();
+            chPath.setApproveButtonText("Save"); //it changes the button title from "Open" to save if this is for a new file
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Documents (*.txt)", "txt", "text");
+            chPath.setFileFilter(filter);
+            int result = chPath.showOpenDialog(null);
+            if(result ==JFileChooser.APPROVE_OPTION){
+                String temp;
+                BufferedWriter objWriter;
+                try {
+                    fileOut = new FileWriter(chPath.getSelectedFile());
+                    objWriter = new BufferedWriter(fileOut);
+                    temp = txtArea.getText();
+                    PrintWriter pw = new PrintWriter(objWriter);
+                    pw.print(temp);
+                    pw.close();
+                    fileOut.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void CopyFile() {
+        if (fileExists) {
+            chPath = new JFileChooser();
+            chPath.setApproveButtonText("Save"); //it changes the button title from "Open" to save if this is for a new file
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Documents (*.txt)", "txt", "text");
+            chPath.setFileFilter(filter);
+            int result = chPath.showOpenDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                String temp;
+                BufferedWriter objWriter;
+                try {
+                    fileOut = new FileWriter(chPath.getSelectedFile());
+                    objWriter = new BufferedWriter(fileOut);
+                    temp = txtArea.getText();
+                    PrintWriter pw = new PrintWriter(objWriter);
+                    pw.print(temp);
+                    pw.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+        else{
+            JOptionPane.showConfirmDialog(MyFrame, "Are you sure you want to clear the text area?","Clear confirmation", 1, 3);
+        }
+    }
+
+    private void ClearAreaAndField() {
+        int exit = JOptionPane.showConfirmDialog(MyFrame, "Are you sure you want to clear the text area?","Clear confirmation", 1, 3);
+        if (exit == JOptionPane.YES_OPTION){
+            fieldPath.setText("");
+            txtArea.setText("");
+        }
+       ;
+    }
+
+    private void ExitApp(){
+        int exit = JOptionPane.showConfirmDialog(MyFrame, "Are you sure you want to exit?","Exit confirmation", 1, 3);
+        if (exit == JOptionPane.YES_OPTION){
+            System.exit(0);
+        }
+    }
+
 }
